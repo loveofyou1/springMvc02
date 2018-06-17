54ec2e8b13faae2381cac8c9a24c4be50744bbc5
sunlei19 <sunlei19@jd.com>
2018-06-17 17:59:25 +0800

    修改log4j2的日志配置文件

--- src/main/java/sun/test/Bar.java
+++ src/main/java/sun/test/Bar.java
@@ -5,9 +5,10 @@ import org.apache.logging.log4j.Logger;
 
 public class Bar {
     static final Logger logger = LogManager.getLogger(Bar.class.getName());
+
     public boolean doIt() {
         logger.entry("BAR");
         logger.error("Did it again!");
-        return logger.exit(false);
+        return logger.traceExit(false);
     }
 }
--- src/main/java/sun/test/HelloWorld.java
+++ src/main/java/sun/test/HelloWorld.java
@@ -9,6 +9,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
+        if (logger.isDebugEnabled()) {
+            logger.debug("日志输出的类名:{},日志级别是:{}", HelloWorld.class, logger.getLevel());
+        }
         logger.error("hello world!");
 
     }
